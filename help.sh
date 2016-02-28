12a69fa5b711d11fae5b525d171bd8cb42acb04e
pamtrak06 <pamtrak06@gmail.com>
2016-02-28 23:41:09 +0100

    correction of missing parameters for help

--- help.sh
+++ help.sh
@@ -1,23 +1,23 @@
 function usage_build()
 {
-  echo -e "edockerbuild"
-  echo -e "\n"
-  echo -e "#configuration"
-  echo -e "- edocker.cfg parameters:"
-  echo -e "  - image_name: name of image to be build"
-  echo -e "  - build_args: arguments give to building image"
-  echo -e "  - build_path: path where to find Dockerfile and his context"
-  echo -e "\n"$
-  echo -e "#usage"
-  echo -e "edockerbuild"
+  echo -e "-- edockerbuild --"
+  echo -e "  # Configuration"
+  echo -e "     - edocker.cfg parameters:"
+  echo -e "       - image_name: name of image to be build"
+  echo -e "       - build_args: arguments give to building image"
+  echo -e "       - build_path: path where to find Dockerfile and his context"
+  echo -e "   # Usage"
+  echo -e "      edockerbuild"
 }
 
 function usage()
 {
-  if [ "$1" = "build" ]; then
+  if [ -z "$1" ]; then
+    echo "Help must have one argument in list: build"
+  elif [ "$1" = "build" ]; then
     usage_build
   fi
 }
 
-usage
+usage $1
 
