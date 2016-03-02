affab0e7d05a12393a3c4badf5f7252a28ea051e
pamtrak06 <pamtrak06@gmail.com>
2016-03-02 22:57:20 +0100

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
 # ----------------------------------------------------
 # The MIT License (MIT)
 #
 # Copyright (c) 2016 copyright pamtrak06@gmail.com
+# ----------------------------------------------------
+# SCRIPT           : clean.sh
+# DESCRIPTION      : clean all images with none attribute
+# CREATOR          : pamtrak06@gmail.com
+# --------------------------------
+# VERSION          : 1.0
+# DATE             : 2016-03-02
+# COMMENT          : creation
+# --------------------------------
+# USAGE            : alias edockerclean
 # ----------------------------------------------------
 
 echo "Images with \"none\" name will be deleted, is it ok for you (y/n) ?"
