d9f2544268f52e0c77473206545b9c7814a0fb76
pamtrak06 <pamtrak06@gmail.com>
2016-03-02 22:47:56 +0100

    Update logs.sh

--- logs.sh
+++ logs.sh
@@ -4,6 +4,16 @@
 #
 # Copyright (c) 2016 copyright pamtrak06@gmail.com
 # ----------------------------------------------------
+# SCRIPT           : logs.sh
+# DESCRIPTION      : docker logs script (read parameters from edocker.cfg)
+# CREATOR          : pamtrak06@gmail.com
+# --------------------------------
+# VERSION          : 1.0
+# DATE             : 2016-03-02
+# COMMENT          : creation
+# --------------------------------
+# USAGE            : alias edockerlogs
+# ----------------------------------------------------
 
 source {edockerpath}/_common.sh
 dockerbasiccontainer "logs" "Logs of container: " "0"
