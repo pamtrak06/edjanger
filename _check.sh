8bc0595ee31fe0e11601ffabc070821556d82872
pamtrak06 <pamtrak06@gmail.com>
2016-03-02 22:49:33 +0100

    Update _check.sh

--- _check.sh
+++ _check.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+# ----------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2016 copyright pamtrak06@gmail.com
+# ----------------------------------------------------
+# SCRIPT           : _check.sh
+# DESCRIPTION      : check path in edocker scripts
+# CREATOR          : pamtrak06@gmail.com
+# --------------------------------
+# VERSION          : 1.0
+# DATE             : 2016-03-02
+# COMMENT          : creation
+# --------------------------------
+# USAGE            : ./_check.sh
+# ----------------------------------------------------
 grep -nrw '.' -e "$PWD"
