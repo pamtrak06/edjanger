b9a7c28b0d897f574cb44115afb13b0e01f956d4
pamtrak06 <pamtrak06@gmail.com>
2016-03-02 23:02:45 +0100

    Update images.sh

--- images.sh
+++ images.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
 # ----------------------------------------------------
 # The MIT License (MIT)
 #
 # Copyright (c) 2016 copyright pamtrak06@gmail.com
+# ----------------------------------------------------
+# SCRIPT           : images.sh
+# DESCRIPTION      : docker images script (read parameters from edocker.cfg)
+# CREATOR          : pamtrak06@gmail.com
+# --------------------------------
+# VERSION          : 1.0
+# DATE             : 2016-03-02
+# COMMENT          : creation
+# --------------------------------
+# USAGE            : alias edockerimages
 # ----------------------------------------------------
 
 source {edockerpath}/_common.sh
