f81694dac02b0f886f1c7d5d22c43c28ae8b2ec8
pamtrak06 <pamtrak06@gmail.com>
2016-03-02 22:46:46 +0100

    Update psa.sh

--- psa.sh
+++ psa.sh
@@ -4,6 +4,16 @@
 #
 # Copyright (c) 2016 copyright pamtrak06@gmail.com
 # ----------------------------------------------------
+# SCRIPT           : psa.sh
+# DESCRIPTION      : docker ps -a script (read parameters from edocker.cfg)
+# CREATOR          : pamtrak06@gmail.com
+# --------------------------------
+# VERSION          : 1.0
+# DATE             : 2016-03-02
+# COMMENT          : creation
+# --------------------------------
+# USAGE            : alias edockerpsa
+# ----------------------------------------------------
 
 source {edockerpath}/_common.sh
 dockerbasiccontainer "ps" "State of containers with name like: " "0"
