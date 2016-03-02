977cc6df185be088965347a8f1eac094fea296f6
pamtrak06 <pamtrak06@gmail.com>
2016-03-02 22:55:23 +0100

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-# ----------------------------------------------------$
-# The MIT License (MIT)$
+# ----------------------------------------------------
+# The MIT License (MIT)
 #
-# Copyright (c) 2016 copyright pamtrak06@gmail.com$
+# Copyright (c) 2016 copyright pamtrak06@gmail.com
+# ----------------------------------------------------
+# SCRIPT           : alias.sh
+# DESCRIPTION      : print edocker aliases
+# CREATOR          : pamtrak06@gmail.com
+# --------------------------------
+# VERSION          : 1.0
+# DATE             : 2016-03-02
+# COMMENT          : creation
+# --------------------------------
+# USAGE            : alias edockeralias
 # ----------------------------------------------------
 
 alias | grep edocker
