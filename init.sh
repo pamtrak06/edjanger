619128ad0a4dc3dea0ec1b9e4d59b959cb9285d0
pamtrak06 <pamtrak06@gmail.com>
2016-03-02 23:03:34 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,16 @@
 #
 # Copyright (c) 2016 copyright pamtrak06@gmail.com
 # ----------------------------------------------------
+# SCRIPT           : init.sh
+# DESCRIPTION      : initailize a edocker.cfg file
+# CREATOR          : pamtrak06@gmail.com
+# --------------------------------
+# VERSION          : 1.0
+# DATE             : 2016-03-02
+# COMMENT          : creation
+# --------------------------------
+# USAGE            : alias edockerinit
+# ----------------------------------------------------
 
 if [ ! -f "edocker.cfg" ] && [ "{edockerpath}" != "$PWD" ]; then
   cp {edockerpath}/edocker.cfg.sample ./edocker.cfg
