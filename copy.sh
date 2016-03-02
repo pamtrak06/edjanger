469fe0b9a73e0524f5e9ed69258641076486f762
pamtrak06 <pamtrak06@gmail.com>
2016-03-02 22:58:37 +0100

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
 # ----------------------------------------------------
 # The MIT License (MIT)
 #
 # Copyright (c) 2016 copyright pamtrak06@gmail.com
+# ----------------------------------------------------
+# SCRIPT           : copy.sh
+# DESCRIPTION      : docker copy script (read parameters from edocker.cfg)
+# CREATOR          : pamtrak06@gmail.com
+# --------------------------------
+# VERSION          : 1.0
+# DATE             : 2016-03-02
+# COMMENT          : creation
+# --------------------------------
+# USAGE            : alias edockercopy
 # ----------------------------------------------------
 
 function usage {
