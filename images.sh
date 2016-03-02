ef9361f4809faed704f89df3f99f686f60b44ba2
pamtrak06 <pamtrak06@gmail.com>
2016-03-02 22:39:40 +0100

    Update images.sh

--- images.sh
+++ images.sh
@@ -4,5 +4,5 @@
 # Copyright (c) 2016 copyright pamtrak06@gmail.com
 # ----------------------------------------------------
 
-source {edockerpath}/common.sh
+source {edockerpath}/_common.sh
 dockerbasicimage "images" "List image which name name contains: "
