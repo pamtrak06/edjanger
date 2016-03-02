0af549b68c3e10cfb391629fcfbb09c53e276f70
pamtrak06 <pamtrak06@gmail.com>
2016-03-02 23:04:09 +0100

    Update inspect.sh

--- inspect.sh
+++ inspect.sh
@@ -4,6 +4,16 @@
 #
 # Copyright (c) 2016 copyright pamtrak06@gmail.com
 # ----------------------------------------------------
+# SCRIPT           : inspect.sh
+# DESCRIPTION      : docker inspect script (read parameters from edocker.cfg)
+# CREATOR          : pamtrak06@gmail.com
+# --------------------------------
+# VERSION          : 1.0
+# DATE             : 2016-03-02
+# COMMENT          : creation
+# --------------------------------
+# USAGE            : alias edockerinspect
+# ----------------------------------------------------
 
 source {edockerpath}/_common.sh
 dockerbasiccontainer "inspect" "Inspection of container: " "0"
