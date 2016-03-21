1d500630b8363bf4d1fb9a791c819e6f768b98ea
pamtrak06 <pamtrak06@gmail.com>
2016-03-21 20:54:21 +0100

    add help command for ports and portdesc, add argument exposed for ports

---
+++ portdesc.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# ----------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2016 copyright pamtrak06@gmail.com
+# ----------------------------------------------------
+# SCRIPT           : ports.sh
+# ALIAS            : edockerports
+# DESCRIPTION      : show standarad ports attribution
+# CREATOR          : pamtrak06@gmail.com
+# --------------------------------
+# VERSION          : 1.0
+# DATE             : 2016-03-02
+# COMMENT          : creation
+# --------------------------------
+# USAGE            : alias edockerlogs
+# ----------------------------------------------------
+source {edockerpath}/_common.sh
+
+if [ -n "$1" ]; then
+  if [[ "$1" =~ ^[-]*h[a-z]* ]] || [ "$1" = "-h" ]; then
+    usage $0  run
+  else
+    echo -e "\n--- Port description for $1"
+    title="Service Name,Port Number,Transport Protocol,Description,Assignee,Contact,Registration Date,Modification Date,Reference,Service Code,Known Unauthorized Uses,Assignment Notes"
+    ports=$(cat {edockerpath}/service-names-port-numbers.csv | grep ",$1,")
+    if [ -n "$ports" ]; then
+      echo -e $title
+      REFIFS=$IFS
+      IFS=$'\n'
+      for p in $ports; do
+        echo "  $p"
+      done
+      IFS=$REFIFS
+    else
+      echo -e "  none"
+    fi
+  fi
+else
+  echo -e "Please set a port number for argument"
+fi
---
+++ ports.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# ----------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2016 copyright pamtrak06@gmail.com
+# ----------------------------------------------------
+# SCRIPT           : ports.sh
+# ALIAS            : edockerports
+# DESCRIPTION      : run command "docker ports" on all running container
+#   ARGUMENT       : exposed -> print list sorted off all container's exposed ports
+# CREATOR          : pamtrak06@gmail.com
+# --------------------------------
+# VERSION          : 1.0
+# DATE             : 2016-03-02
+# COMMENT          : creation
+# --------------------------------
+# USAGE            : alias edockerlogs
+# ----------------------------------------------------
+source {edockerpath}/_common.sh
+
+ports=
+if [[ "$1" =~ ^[-]*h[a-z]* ]] || [ "$1" = "-h" ]; then
+  usage $0  run
+else
+  # list all container
+  containers=$(docker ps --filter "status=running" -aq)
+  if [ "$1" != "exposed" ]; then
+    echo -e "--- List all mapped ports on runnning containers..."
+  else
+    echo -e "--- List all exposed ports on runnning containers..."
+  fi
+  for c in ${containers}; do
+    id=$(docker ps --format "Id:{{.ID}} Image:{{.Image}} Name:{{.Names}}" |grep $c)
+    if [ "$1" != "exposed" ]; then
+      echo -e "$id"
+      docker port $c
+     else
+       ports+=$(printf "%s;" $(docker port $c|cut -d ':' -f2))
+     fi
+  done
+  if [ "$1" = "exposed" ]; then
+    echo -e "${ports%;;}"
+  fi
+fi
+
