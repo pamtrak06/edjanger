d1fd420a59e61e72b20ebae71cfa763d6f60e6ea
pamtrak06 <pamtrak06@gmail.com>
2016-07-22 20:58:27 +0200

    at initailisation of edocker project add mkdir /build and Dockerfile

--- init.sh
+++ init.sh
@@ -20,10 +20,19 @@ if [[ "$1" =~ ^[-]*h[a-z]* ]] || [ "$1" = "-h" ]; then
   usage $0 init
 else
   if [ ! -f "edocker.${config_extension}" ] && [ "{edockerpath}" != "$PWD" ]; then
+    echo -e "> Initialize edocker configuration file: edocker.${config_extension} ..."
     cp {edockerpath}/edocker_template.${config_extension} ./edocker.${config_extension}
   else
     source {edockerpath}/_common.sh
     echo -e "File edocker.${config_extension} is already in your current directory !"
     checkconfig
+  fi
+  if [ ! -d "build" ] && [ "{edockerpath}" != "$PWD" ]; then
+    echo -e "> Initialize edocker configuration folder for Dockerfile: /build ..."
+    mkdir build/
+  fi
+  if [ ! -f "build/Dockerfile" ] && [ "{edockerpath}" != "$PWD" ]; then
+    echo -e "> Initialize Dockerfile: build/Dockerfile ..."
+    touch build/Dockerfile
   fi
 fi
