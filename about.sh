39aa171ca2d15ac62a7e88a44e75db4cdfd94bf3
pamtrak06 <pamtrak06@gmail.com>
2016-08-03 13:04:29 +0200

    add about.sh script

--- about.sh
+++ about.sh
@@ -17,11 +17,11 @@
 # USAGE            : edockerabout
 # ----------------------------------------------------
 
-echo "        _            _              "
-echo " ___  __| | ___   ___| | _____ _ __ "
-echo "/ _ \/  _`|/  _\ / __| |/ / _ | '__|"
-echo "|  __| (_| | (_) | (_|  |   __| |   "
-echo "\___|\__,_|\___/ \___|_|\_\___|_|   "
-echo ""
-echo "Licence: The MIT License (MIT)"
-echo "Copyright (c) 2016 copyright pamtrak06@gmail.com"
+echo -e "        _            _              "
+echo -e " ___  __| | ___   ___| | _____ _ __ "
+echo -e "/ _ \/  _ |/  _\ / __| |/ / _ | '__|"
+echo -e "|  __| (_| | (_) | (_|   || __| |   "
+echo -e "\___|\__,_|\___/ \___|_|\_\___|_|   "
+echo -e ""
+echo -e "Licence: The MIT License (MIT)"
+echo -e "Copyright (c) 2016 copyright pamtrak06@gmail.com"
