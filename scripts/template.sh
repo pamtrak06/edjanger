4297bd8c93db5bbafacf18f09e319348c40f1e81
pamtrak06 <pamtrak06@gmail.com>
2016-11-11 01:54:33 +0100

    update templating: add properties=<properties name> syntax

--- scripts/template.sh
+++ scripts/template.sh
@@ -17,71 +17,117 @@
 # ----------------------------------------------------
 source {edockerpath}/_common.sh
 
+function check_export_presence_from_properties()
+{
+  prop_file=$1
+
+  nbvar=$(cat $prop_file |grep "="| wc -l)
+  nbexp=$(cat $prop_file |grep "export"| wc -l)
+
+  if [ "$nbvar" = "$nbexp" ]; then
+    check_export_presence_from_properties=true;
+  else
+    echo -e "edocker:ERROR one or several \"export\" command are absent(s) in the previous properties, please fix it"
+    echo -e "\tvariable declaration must have this form: \"export <variable name>=<value>\""
+    echo -e "\tcontent of the previous properties:"
+    while read line; do echo -e "\t\t$line"; done < $prop_file
+    check_export_presence_from_properties=false;
+  fi
+}
+
 function create_edocker_properties()
 {
-  configuration=$1
+  command=$1
+
+  if [[ ! "$command" == *"properties="* ]]; then
+    echo -e "edocker:ERROR argument must be set with following form: properties=\"<basename or properties name>\"."
+    exit -1
+  else
+    configuration=${command##properties=}
+  fi
 
   if [ ! "${configuration##*.}" = "properties" ]; then
-    configuration=$1.properties
+    configuration=$configuration.properties
   fi
 
   if [ "${configuration}" = "edocker" ]; then
     echo -e "edocker:ERROR this name could not be used for this purpose. Please choose another one."
+    exit -1
   fi
 
   # Retrieve all edocker configuration files from current directory
   listconf=$(find $PWD -name "edocker.template")
   if [ -z "$listconf" ]; then
     echo -e "edocker:ERROR No edocker.template available recursively in this directory"
+    exit -1
   fi
 
   for template in ${listconf[@]}
   do
+
     echo -e "edocker:INFO Process informations of template: \"${template}\"  ..."
 
     continue=true
 
     # one main configuration file
     if [ -n "${configuration}" ] && [ -f "${configuration}" ]; then
+
       prop_file=${configuration}
       echo -e "edocker:INFO Use main configuration file: \"${prop_file}\"  ..."
+      check_export_presence_from_properties "$prop_file"
+      continue=$?
+
     # one configuration file by edocker.template
     elif [ -n "${configuration}" ]; then
+
       working_directory=$(dirname ${template})
       prop_file=${working_directory}/${configuration}
       echo -e "edocker:INFO Use local configuration file: \"${prop_file}\"  ..."
+      check_export_presence_from_properties "$prop_file"
+      continue=$?
+
     else
+
       continue=false
       echo -e "edocker:ERROR File configuration name must exist and be set as argument"
+
     fi
 
-    if [ "${continue}" = "true" ]; then
+    if [ ${continue} ]; then
 
       # replace variable from configuration file in template
       # if configuration file exist (global or local)
       if [ -f "${prop_file}" ]; then
+
         edockerproperties=${template%.template}.properties
-        echo "edockerproperties: ${edockerproperties}"
+
         # check if edocker.properties exist
         if [ -f "${edockerproperties}" ]; then
+
           echo -e "edocker:WARNING edocker properties file \"${edockerproperties}\" already exist, do you want to replace it (y/n) ?"
           read response
+
           if [ "y" = "${response}" ]; then
             date_time=$(date +"%Y%m%d_%H%M%S")
             cp ${edockerproperties} ${template%.template}_${date_time}.bak
             echo -e "edocker:INFO create \"${edockerproperties}\" file from template \"${template}\" and configuration file \"${prop_file}\""
             source ${prop_file} && envsubst < "${template}" | tee "${edockerproperties}" > /dev/null
           fi
+
         # else create edocker.properties
         else
+
           echo -e "edocker:INFO create \"${edockerproperties}\" file from template \"${template}\" and configuration file \"${prop_file}\""
           source ${prop_file} && envsubst < "${template}" | tee "${edockerproperties}" > /dev/null
+
         fi
       # else configuration file doe not exist
       else
         echo -e "edocker:ERROR properties file (${prop_file}) does not exist for template ${template}, edocker.properties could not be created"
       fi
 
+    else
+      echo -e "edocker:ERROR templating aborted, see previous reported errors"
     fi
 
   done
--- scripts/templates/templating/deploy_run.sh
+++ scripts/templates/templating/deploy_run.sh
@@ -38,24 +38,37 @@ if [ $bitwise -ge 0 ]; then
   rm -f web2/edocker.properties
   rm -f web1/*.bak
   rm -f web2/*.bak
+  rm -f edocker.log
 fi
 
 bitwise=$(echo "$step / 2" |bc)
 if [ $bitwise -ne 0 ]; then
   echo "#2 generate edocker.properties from template and properties..."
-  edockertemplate $mode
+  edockertemplate properties=$mode 2>&1 | tee edocker.log
+  if [ -n "$(cat edocker.log|grep ERR)" ]; then
+    rm -f edocker.log
+    exit -1
+  fi
 fi
 
 bitwise=$(echo "$step / 3" |bc)
 if [ $bitwise -ne 0 ]; then
   echo "#3 compare we1 and web2 configuration..."
-  ./diff_conf.sh
+  ./diff_conf.sh 2>&1 | tee edocker.log
+  if [ -n "$(cat edocker.log|grep ERR)" ]; then
+    rm -f edocker.log
+    exit -1
+  fi
 fi
 
 bitwise=$(echo "$step / 4" |bc)
 if [ $bitwise -ne 0 ]; then
   echo "#4 generate docker-compose.yaml from edocker.properties..."
-  edockercompose
+  edockercompose 2>&1 | tee edocker.log
+  if [ -n "$(cat edocker.log|grep ERR)" ]; then
+    rm -f edocker.log
+    exit -1
+  fi
 fi
 
 bitwise=$(echo "$step / 5" |bc)
@@ -63,9 +76,29 @@ if [ $bitwise -ne 0 ]; then
   echo "#5 run docker compose"
   status=$(docker-compose ps|grep "web")
   if [ -n "$status" ]; then
-    docker-compose -p "$mode" stop
+    docker-compose -p "$mode" stop 2>&1 | tee edocker.log
+    if [ -n "$(cat edocker.log|grep ERR)" ]; then
+      rm -f edocker.log
+      exit -1
+    fi
   fi
-  docker-compose -p "$mode" rm
-  docker-compose -p "$mode" build
-  docker-compose -p "$mode" up
+  docker-compose -p "$mode" rm 2>&1 | tee edocker.log
+  if [ -n "$(cat edocker.log|grep ERR)" ]; then
+    rm -f edocker.log
+    exit -1
+  fi
+
+  docker-compose -p "$mode" build 2>&1 | tee edocker.log
+  if [ -n "$(cat edocker.log|grep ERR)" ]; then
+    rm -f edocker.log
+    exit -1
+  fi
+
+  docker-compose -p "$mode" up 2>&1 | tee edocker.log
+  if [ -n "$(cat edocker.log|grep ERR)" ]; then
+    rm -f edocker.log
+    exit -1
+  fi
+
+  rm -f edocker.log
 fi
