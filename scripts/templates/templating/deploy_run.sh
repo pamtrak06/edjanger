9b863b91241d696be2a1ef8d67fb2da016269fcb
pamtrak06 <pamtrak06@gmail.com>
2016-11-16 18:19:55 +0100

    Update deploy_run.sh

--- scripts/templates/templating/deploy_run.sh
+++ scripts/templates/templating/deploy_run.sh
@@ -18,87 +18,79 @@ function usage(){
   return -1
 }
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 2 ] || [ -z "$mode" ] || ([ ! "$mode" = "production" ] && [ ! "$mode" = "integration" ]); then
   usage
-fi
-
-if [ -z "$mode" ]; then
-  usage
-fi
-
-if [ ! "$mode" = "production" ] && [ ! "$mode" = "integration" ]; then
-  usage
-fi
-
-bitwise=$(echo "$step / 1" |bc)
-if [ $bitwise -ge 0 ]; then
-  echo "#1 cleaning edocker.properties..."
-  rm -f docker-compose.yaml
-  rm -f web1/edocker.properties
-  rm -f web2/edocker.properties
-  rm -f web1/*.bak
-  rm -f web2/*.bak
-  rm -f edocker.log
-fi
-
-bitwise=$(echo "$step / 2" |bc)
-if [ $bitwise -ne 0 ]; then
-  echo "#2 generate edocker.properties from template and properties..."
-  edockertemplate properties=$mode 2>&1 | tee edocker.log
-  if [ -n "$(cat edocker.log|grep ERR)" ]; then
+else
+  bitwise=$(echo "$step / 1" |bc)
+  if [ $bitwise -ge 0 ]; then
+    echo "#1 cleaning edocker.properties..."
+    rm -f docker-compose.yaml
+    rm -f web1/edocker.properties
+    rm -f web2/edocker.properties
+    rm -f web1/*.bak
+    rm -f web2/*.bak
     rm -f edocker.log
-    step=-1
   fi
-fi
 
-bitwise=$(echo "$step / 3" |bc)
-if [ $bitwise -ne 0 ]; then
-  echo "#3 compare web1 and web2 configuration..."
-  ./diff_conf.sh 2>&1 | tee edocker.log
-  if [ -n "$(cat edocker.log|grep ERR)" ]; then
-    rm -f edocker.log
-    step=-1
-  fi
-fi
-
-bitwise=$(echo "$step / 4" |bc)
-if [ $bitwise -ne 0 ]; then
-  echo "#4 generate docker-compose.yaml from edocker.properties..."
-  edockercompose 2>&1 | tee edocker.log
-  if [ -n "$(cat edocker.log|grep ERR)" ]; then
-    rm -f edocker.log
-    step=-1
-  fi
-fi
-
-bitwise=$(echo "$step / 5" |bc)
-if [ $bitwise -ne 0 ]; then
-  echo "#5 run docker compose"
-  status=$(docker-compose ps|grep "web")
-  if [ -n "$status" ]; then
-    docker-compose -p "$mode" stop 2>&1 | tee edocker.log
+  bitwise=$(echo "$step / 2" |bc)
+  if [ $bitwise -ne 0 ]; then
+    echo "#2 generate edocker.properties from template and properties..."
+    edockertemplate properties=$mode 2>&1 | tee edocker.log
     if [ -n "$(cat edocker.log|grep ERR)" ]; then
       rm -f edocker.log
       step=-1
     fi
   fi
-  docker-compose -p "$mode" rm 2>&1 | tee edocker.log
-  if [ -n "$(cat edocker.log|grep ERR)" ]; then
-    rm -f edocker.log
-    step=-1
+
+  bitwise=$(echo "$step / 3" |bc)
+  if [ $bitwise -ne 0 ]; then
+    echo "#3 compare web1 and web2 configuration..."
+    ./diff_conf.sh 2>&1 | tee edocker.log
+    if [ -n "$(cat edocker.log|grep ERR)" ]; then
+      rm -f edocker.log
+      step=-1
+    fi
   fi
 
-  docker-compose -p "$mode" build 2>&1 | tee edocker.log
-  if [ -n "$(cat edocker.log|grep ERR)" ]; then
-    rm -f edocker.log
-    step=-1
+  bitwise=$(echo "$step / 4" |bc)
+  if [ $bitwise -ne 0 ]; then
+    echo "#4 generate docker-compose.yaml from edocker.properties..."
+    edockercompose 2>&1 | tee edocker.log
+    if [ -n "$(cat edocker.log|grep ERR)" ]; then
+      rm -f edocker.log
+      step=-1
+    fi
   fi
 
-  docker-compose -p "$mode" up 2>&1 | tee edocker.log
-  if [ -n "$(cat edocker.log|grep ERR)" ]; then
-    rm -f edocker.log
-    step=-1
-  fi
+  bitwise=$(echo "$step / 5" |bc)
+  if [ $bitwise -ne 0 ]; then
+    echo "#5 run docker compose"
+    status=$(docker-compose ps|grep "web")
+    if [ -n "$status" ]; then
+      docker-compose -p "$mode" stop 2>&1 | tee edocker.log
+      if [ -n "$(cat edocker.log|grep ERR)" ]; then
+        rm -f edocker.log
+        step=-1
+      fi
+    fi
+    docker-compose -p "$mode" rm 2>&1 | tee edocker.log
+    if [ -n "$(cat edocker.log|grep ERR)" ]; then
+      rm -f edocker.log
+      step=-1
+    fi
 
-  rm -f edocker.log
+    docker-compose -p "$mode" build 2>&1 | tee edocker.log
+    if [ -n "$(cat edocker.log|grep ERR)" ]; then
+      rm -f edocker.log
+      step=-1
+    fi
+
+    docker-compose -p "$mode" up 2>&1 | tee edocker.log
+    if [ -n "$(cat edocker.log|grep ERR)" ]; then
+      rm -f edocker.log
+      step=-1
+    fi
+
+    rm -f edocker.log
+  fi
 fi
