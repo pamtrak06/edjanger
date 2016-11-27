88cba5b9efb7fc1b2f4142fdf08d26de91748f9e
pamtrak06 <pamtrak06@gmail.com>
2016-11-27 15:18:12 +0100

    header refactoring

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,12 +6,20 @@
 ##     @script.name [option]
 ##  
 ##  Options:
-##     -h, --help                     print this documentation.
+##     -h, --help                       print this documentation.
 ##
-##         --index=INDEX              index of the container name.
+##         --index=INDEX                index of the container name.
 ##  
 ##  Parameters (edjanger.properties):
-##     run_options                    other run options.
+##     exposed_ports                    exposed port.
+##     command_run                      show docker command when edjanger is used.
+##     environment_variables            environnment variables.
+##     image_name                       image name.
+##     linked_containers                linked container.
+##     network_settings                 all network settings options.
+##     run_other_options                all other available options for docker run.
+##     runtime_constraints_on_resources runtime constraints on resources.
+##     volumes_from                     expose volumes from another container into current container.
 ##  
 ##  edjanger, The MIT License (MIT)
 ##  Copyright (c) 2016 copyright pamtrak06@gmail.com
@@ -37,6 +45,11 @@
 source {edjangerpath}/_common.sh
 
 read_app_properties
+
+# check required configuration
+[ -z "${image_name}" ]                         && echo "Image name must be filled, configure variable image_name in edjanger.${config_extension}" && exit -1
+[ -z "${container_name}" ]                     && echo "Container name must be filled, configure variable container_name in edjanger.${config_extension}" && exit -1
+
 [ -n "${run_other_options}" ]                  && commandoptions="${commandoptions} ${run_other_options}"
 [ -n "${network_settings}" ]                   && commandoptions="${commandoptions} ${network_settings}"
 [ -n "${runtime_constraints_on_resources}" ]   && commandoptions="${commandoptions} ${runtime_constraints_on_resources}"
