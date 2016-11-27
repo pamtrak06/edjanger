c70b5166040bb4d3e8bc42d9b006f847c3b0a194
pamtrak06 <pamtrak06@gmail.com>
2016-11-27 15:17:10 +0100

    deep refactoring of build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,46 +8,58 @@
 ##  Options:
 ##     -h, --help                     print this documentation.
 ##  
-##         --index=INDEX              index of the container name.
-##  
 ##  Parameters (edjanger.properties):
-##     image_name                     image name
-##     proxy_args                     proxy variables (HTTP_PROXY,HTTPS_PROXY,http_proxy,https_proxy) read from proxy.zip or environment
 ##     build_args                     build arguments
 ##     build_forcerm                  always remove intermediate containers
 ##     build_rm                       remove intermediate containers after a successful build
 ##     build_nocache                  do not use cache when building the image
 ##     build_file                     name of the Dockerfile (Default is 'CURRENT PATH/Dockerfile')
 ##     build_path                     path where is found Dockerfile and its dependencies
+##     image_name                     image name
+##  
+##  Environement:
+##     proxy_args                     proxy variables (HTTP_PROXY,HTTPS_PROXY,http_proxy,https_proxy) read from proxy.zip or environment
 ##  
 ##  edjanger, The MIT License (MIT)
 ##  Copyright (c) 2016 copyright pamtrak06@gmail.com
 ##  
 # ------------------------------------------------------------------------------
+###
+### External options:
+###    -h, --help                     print this documentation.
+###  
+### Internal options:
+###  
+###        --script=SCRIPT            name of the main script
+###  
+###        --command=COMMAND          name of the docker command to execute
+###  
+###        --commandcomment=COMMAND  printed comment of the command to execute
+###  
+###        --commandoptions=OPTIONS  options read in the edjanger.properties
+###  
+# ------------------------------------------------------------------------------
 source {edjangerpath}/_common.sh
 
-if [[ "$1" =~ ^[-]*h[a-z]* ]] || [ "$1" = "-h" ]; then
-  #usage $0 build
-  printHeader $0
-else
-  rename_edocker_properties
-  if [ ! -f edjanger.${config_extension} ]; then
-    echo -e "edjanger:ERROR No edjanger.${config_extension} available, use \"<edjangerinit>\" command to initialize one in this directory"
-  else
-    read_app_properties
-    echo build image_name: ${image_name}...
-    if [ ! -d ${build_path} ]; then
-      echo "Build path must exist and be a folder, configure variable build_path in edjanger.${config_extension}"
-    else
-      . {edjangerpath}/_proxy.sh
-      build_arguments="${proxy_args} ${build_args}"
-      docker build -t "${image_name}" ${build_arguments} ${build_forcerm} ${build_rm} ${build_nocache} ${build_file} ${build_path}
-      if [ "true" = "${docker_command}" ]; then
-        echo -e "> Executed docker command:"
-        echo -e "> docker build -t \"${image_name}\" ${build_arguments} ${build_forcerm} ${build_rm} ${build_nocache} ${build_file} ${build_path}"
-      fi
-      build_arguments=""
-      . {edjangerpath}/_proxy_reset.sh
-    fi
-  fi
-fi
+read_app_properties
+
+# check required configuration
+[ ! -d "${build_path}" ]                       && echo "Build path must exist and be a folder, configure variable build_path in edjanger.${config_extension}" && exit -1
+[ -z "${image_name}" ]                         && echo "Image name must be filled, configure variable image_name in edjanger.${config_extension}" && exit -1
+
+. {edjangerpath}/_proxy.sh
+build_arguments="${proxy_args} ${build_args}"
+
+[ -n "${build_arguments}" ]                    && commandoptions="${commandoptions} ${build_arguments}"
+[ -n "${build_forcerm}" ]                      && commandoptions="${commandoptions} ${build_forcerm}"
+[ -n "${build_rm}" ]                           && commandoptions="${commandoptions} ${build_rm}"
+[ -n "${build_nocache}" ]                      && commandoptions="${commandoptions} ${build_nocache}"
+[ -n "${build_file}" ]                         && commandoptions="${commandoptions} ${build_file}"
+[ -n "${image_name}" ]                         && commandoptions="${commandoptions} ${image_name}"
+[ -n "${build_path}" ]                         && commandoptions="${commandoptions} ${build_path}"
+[ -n "${commandoptions}" ]                     && commandoptions="--commandoptions=\"${commandoptions}\""
+dockerbasicimage "--scriptname=\"$0\";--command=\"build -t\";--commandcomment=\"Build image: {image_name}...\";${commandoptions};$@"
+
+build_arguments=""
+. {edjangerpath}/_proxy_reset.sh
+
