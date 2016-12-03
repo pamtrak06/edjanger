224ee1018182c157b53d596e5c33104acf8c7569
pamtrak06 <pamtrak06@gmail.com>
2016-12-03 19:01:09 +0100

    correction of shell call

--- scripts/exec.sh
+++ scripts/exec.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-##  Run command "docker exec" with parameters read from edjanger.properties. 
+##  Description: run command "docker exec" with parameters read from edjanger.properties. 
 ##  File edjanger.properties must be present in path.
 ##  By default enter in last container if no index specified.
 ##  
@@ -48,12 +48,12 @@ read_app_properties
 # check required configuration
 [ -z "${container_name}" ]          && echo "Container name must be filled, configure variable container_name in edjanger.${config_extension}" && exit -1
 
-#[ -n "${container_name}" ]         && commandoptions="${commandoptions} ${container_name}"
 [ -n "${command_run}" ]             && commandoptions="${commandoptions} ${command_run}"
+[ -n "${container_name}" ]          && commandoptions="${commandoptions} {container_name}"
 [ -z "${command_run}" ]             && commandoptions="${commandoptions} /bin/bash"
 [ -n "${commandoptions}" ]          && commandoptions="--commandoptions=\"${commandoptions}\""
 [ -n "$@" ]                         && externaloptions=$(echo $@ | sed "s|[[:space:]](.*)=(.*)|;$1=$2|g") \
                                     && externaloptions=$(echo $externaloptions | sed "s|[[:space:]]--|;--|g") \
                                     && externaloptions=$(echo $externaloptions | sed "s|[[:space:]]-|;-|g")
-dockerbasiccontainer "--scriptname=\"$0\";--commandline=\"exec -it {container_name}\";--commandcomment=\"Enter in container: {container_name}...\";${commandoptions};${externaloptions}"
+dockerbasiccontainer "--scriptname=\"$0\";--commandline=\"exec -it\";--commandcomment=\"Enter in container: {container_name}...\";${commandoptions};${externaloptions}"
 
