a400b0706ace21f69fc8a68dbaa86d05afcddd95
pamtrak06 <pamtrak06@gmail.com>
2016-12-04 18:42:34 +0100

    init: add option to init with a template

--- scripts/init.sh
+++ scripts/init.sh
@@ -13,6 +13,8 @@
 ##  Options:
 ##     -h, --help                     print this documentation
 ##  
+##         --template=httpd           initialize with a "httpd" template
+##  
 ##  edjanger, The MIT License (MIT)
 ##  Copyright (c) 2016 copyright pamtrak06@gmail.com
 ##  
@@ -22,9 +24,10 @@ checkinstall=$(cat $0|grep -v checkinstall|grep "edjangerpath")
 
 source {edjangerpath}/_common.sh
 
-if [[ "$1" =~ ^[-]*h[a-z]* ]] || [ "$1" = "-h" ]; then
-  printHeader $0
-else
+[[ -n "$@" ]]                       && externaloptions=$(echo $@)
+
+function initialize() 
+{
   echo -e "> Initialize edjanger project ..."
   if [ ! -f "edjanger.template" ] && [ "{edjangerpath}" != "$PWD" ]; then
     echo -e "  . Initialize edjanger template file example: edjanger.template ..."
@@ -55,5 +58,27 @@ else
   if [ ! -f "build/Dockerfile" ] && [ "{edjangerpath}" != "$PWD" ]; then
     echo -e "  . Initialize Dockerfile: build/Dockerfile ..."
     touch build/Dockerfile
+  fi
+}
+
+function templateHttpd() 
+{
+  echo "FROM httpd" > build/Dockerfile
+  sed -e "s/\(image_name=\).*/\1\"test\/webserver\"/" configuration.properties > configuration.tmp  && mv configuration.tmp configuration.properties
+  sed -e "s/\(container_name=\).*/\1\"webtest\"/" configuration.properties > configuration.tmp  && mv configuration.tmp configuration.properties
+  sed -e "s/\(export app_exposed_ports=\)\".*\"/\1\"-p 80:80 -p 443:443\"/" configuration.properties > configuration.tmp  && mv configuration.tmp configuration.properties
+  sed -e "s/\(export app_shared_volumes=\)\".*\"/\1\"-v \\\$PWD\/volumes\/html:\/usr\/local\/apache2\/htdocs -v \\\$PWD\/volumes\/logs:\/usr\/local\/apache2\/logs\"/" configuration.properties > configuration.tmp  && mv configuration.tmp configuration.properties
+  sed -e "s/\(export app_environment_variables=\.*\)/#\1/" configuration.properties > configuration.tmp  && mv configuration.tmp configuration.properties
+  sed -e "s/\(container_hostname=\.*\)/#\1/" edjanger.template > edjanger.template.tmp  && mv edjanger.template.tmp edjanger.template
+  sed -e "s/\(environment_variables=\.*\)/#\1/" edjanger.template > edjanger.template.tmp  && mv edjanger.template.tmp edjanger.template
+  . {edjangerpath}/template.sh --properties=configuration.properties
+}
+
+if [[ "$1" =~ ^[-]*h[a-z]* ]] || [ "$1" = "-h" ]; then
+  printHeader $0
+else
+  initialize
+  if [[ "${externaloptions}" == *"template=httpd"* ]]; then
+    templateHttpd
   fi
 fi
